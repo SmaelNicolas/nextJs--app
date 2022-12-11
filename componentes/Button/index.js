8bb68d4fee58e4b85d3b37e8734e5bc05d899135
Smael Nicolas <nico.smael@gmail.com>
2022-12-11 16:59:36 -0300

    add button styles

--- componentes/Button/index.js
+++ componentes/Button/index.js
@@ -6,13 +6,26 @@ export default function Button({ children, onClick }) {
 			<button onClick={onClick}>{children}</button>
 			<style jsx>{`
 				button {
+					display: flex;
+					align-items: center;
 					margin: 16px;
 					background: ${colors.black};
 					border: 0;
 					color: #fff;
 					border-radius: 9999px;
-					font-weight: 800;
+					font-weight: 700;
+					font-size: 16px;
+					cursor: pointer;
 					padding: 8px 24px;
+					transition: opacity 0.3s ease;
+				}
+
+				button > :global(svg) {
+					margin-right: 7px;
+				}
+
+				button:hover {
+					opacity: 0.7;
 				}
 			`}</style>
 		</>
